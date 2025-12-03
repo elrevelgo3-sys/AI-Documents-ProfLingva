89c0cb3d6b10d7e7a983c09d29d0bbc025c8d09b
elrevelgo3-sys <elrevelgo3@gmail.com>
2025-12-03 21:31:42 +0700

    Update TextToSpeech.tsx

--- components/TextToSpeech.tsx
+++ components/TextToSpeech.tsx
@@ -1,2 +1,8 @@
-// Component removed
-export default () => null;
+// components/TextToSpeech.tsx
+import React from 'react';
+
+const TextToSpeech: React.FC = () => {
+  return <div>Text to Speech Coming Soon</div>;
+};
+
+export default TextToSpeech;
