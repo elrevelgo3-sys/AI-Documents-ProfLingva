a8ebe03b923ae37df12caf41c996bb58c6fa8946
elrevelgo3-sys <elrevelgo3@gmail.com>
2025-12-03 21:31:28 +0700

    Update ImageGenerator.tsx

--- components/ImageGenerator.tsx
+++ components/ImageGenerator.tsx
@@ -1,2 +1,8 @@
-// Component removed
-export default () => null;
+// components/ImageGenerator.tsx
+import React from 'react';
+
+const ImageGenerator: React.FC = () => {
+  return <div>Image Generator Coming Soon</div>;
+};
+
+export default ImageGenerator;
